340375bd6b253942f6b13968acaf5b6bfe25bc78
franciscodiazdev <63429666+franciscodiazdev@users.noreply.github.com>
2020-04-09 19:02:20 -0400

    Add files via upload

---
+++ introduccion.php
@@ -0,0 +1,18 @@
+
+
+<?php
+
+$variable1 ="Hola Mundo!";      /* Esta variable contiene elementos de
+                                tipo string (texto) */
+
+$variable2 =0;                  /* Variable de tipo number puede ser
+                                int, float, boolean */
+
+echo $variable1;                /* Imprime en pantalla - hola Mundo - */
+
+echo $variable2;                /* Imprime en pantalla  - 0 - */
+
+
+
+?>
+
